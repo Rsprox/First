4c1116487fe79354e97dd377e61644f0e6a7239f
Rsprox <60698490+Rsprox@users.noreply.github.com>
2020-02-05 22:12:02 +0500

    Update Option_2.py

--- DS - test tasks/1/Option_2.py
+++ DS - test tasks/1/Option_2.py
@@ -1,4 +1,4 @@
 import re
 input_list = ["Т356ОК56", "Т357ОК", "A123AA11", "А222АА123", "A12AA123", "A123CC1234", "AA123A12", "Т359ОК59", "У0019ОК1", "Х9999ОХ"]
-result = [x for x in input_list if re.fullmatch(r'[АВЕКМНОРСТУХ]{1}\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}', x)]
+result = [x for x in input_list if re.fullmatch(r'[АВЕКМНОРСТУХ]{1}\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}', x)]    #Используются русские бувы
 print(*result)
